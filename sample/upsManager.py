6ad81c8b3604f0c2bdc1f3744941c2b1566339d1
smurfolan <kristian.azmanov@gmail.com>
2019-03-18 20:04:35 +0200

    Update relative paths to absolute

--- sample/upsManager.py
+++ sample/upsManager.py
@@ -13,7 +13,7 @@ class UpsManager():
     __INPUT_GPIO_PIN_NUMBER = 11
     __CHECK_FREQUENCY = 5
     __MINIMAL_LOW_BAT_DET_TIMEGAP_ALLOWED = 5.5
-    __LOG_FILE_NAME = "lowBatteryLog.txt"
+    __LOG_FILE_NAME = "/home/pi/Desktop/PeepNee/sample/lowBatteryLog.txt"
     
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
@@ -33,7 +33,7 @@ class UpsManager():
     def __periodicallyCheckBatteryLevel(self):
         while getattr(self.worker_thread, "do_run", True):
             # -- Low battery was detected -- #
-            if not GPIO.input(self.__INPUT_GPIO_PIN_NUMBER):
+            if not GPIO.input(self.__INPUT_GPIO_PIN_NUMBER):         
                 self.__log_low_battery_entry()
                 
                 if self._lastTimeLowBatteryDetected is None:
@@ -50,6 +50,7 @@ class UpsManager():
     def __start_shutdown_procedure(self):
         f = open(self.__LOG_FILE_NAME, "a")
         f.write("The mailbox is going to shut down now!\n")
+        f.flush()
         f.close()
         
         fbManager = FirebaseManager()
@@ -60,6 +61,7 @@ class UpsManager():
     def __log_low_battery_entry(self):
         f = open(self.__LOG_FILE_NAME, "a")
         f.write("[" + str(datetime.datetime.now()) + "] LOW BATTERY!\n")
+        f.flush()
         f.close()
 
 # Usage example
@@ -69,5 +71,3 @@ class UpsManager():
 # sleep(40)
 # upsm.sleep()
 # print('UPS manager is now in sleep state.')
-
-
